9c380b64087b25c4757f46f7c00d0c20ea36f398
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-10-01 13:07:26 -0500

    Iniciando con proyecto de maquina de dulces:
    Trabajando con entidades y objetos de valor

---
+++ DddInPractice.Logic/Money.cs
@@ -0,0 +1,68 @@
+﻿namespace DddInPractice.Logic
+{
+    public class Money : ValueObject<Money>
+    {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount
+        )
+        {
+            OneCentCount += oneCentCount;
+            TenCentCount += tenCentCount;
+            QuarterCount += quarterCount;
+            OneDollarCount += oneDollarCount;
+            FiveDollarCount += fiveDollarCount;
+            TwentyDollarCount += twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+            );
+
+            return sum;
+        }
+
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount
+                && TenCentCount == other.TenCentCount
+                && QuarterCount == other.QuarterCount
+                && OneDollarCount == other.OneDollarCount
+                && FiveDollarCount == other.FiveDollarCount
+                && TwentyDollarCount == other.TwentyDollarCount;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashCode = OneCentCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuarterCount;
+                hashCode = (hashCode * 397) ^ OneDollarCount;
+                hashCode = (hashCode * 397) ^ FiveDollarCount;
+                hashCode = (hashCode * 397) ^ TwentyDollarCount;
+
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,35 @@
+﻿namespace DddInPractice.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
+
+        public int OneCentCountInTransaction { get; private set; }
+        public int TenCentCountInTransaction { get; private set; }
+        public int QuarterCountInTransaction { get; private set; }
+        public int OneDollarCountInTransaction { get; private set; }
+        public int FiveDollarCountInTransaction { get; private set; }
+        public int TwentyDollarCountInTransaction { get; private set; }
+
+        public void InsertMoney(
+            Money money
+        )
+        {
+            MoneyInTransaction = money;
+        }
+
+        public void ReturnMoney()
+        {
+            //MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+
+            //MoneyInTransaction = 0;
+        }
+
+    }
+}
